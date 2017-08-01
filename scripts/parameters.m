7f3786d71169d363872dabcc609fae9dfe72904f
mitchell <mitchell@LinuxBox>
2017-08-01 16:31:08 -0400

    Fixed coordinate transformations, as well as translational force calculations on main wing.

--- scripts/parameters.m
+++ scripts/parameters.m
@@ -24,7 +24,7 @@ p.viscosity = 1.4207E-5; % Kinematic viscosity of air
 p.g         = 9.80665;
 
 % Initial Conditions
-p.initPositionGFS  = [100 0  (10*pi/180)   ]; % Initial position in spherical coordinates
+p.initPositionGFS  = [100 0  (45*pi/180)   ]; % Initial position in spherical coordinates
 p.initVelocity  = 10;
 p.initTwist     = 0*(pi/180);
 p.initOmega     = 0;
@@ -36,10 +36,30 @@ p.J               = (p.mass*p.wingSpan^2)/12; % Rotational inertia about body fi
 p.refAreaRudder   = p.refLengthRudder*p.rudderSpan;
 % Initial velocity in GFS
 p.initVelocityGFS(1) = 0 ;
-p.initVelocityGFS(2) = (p.initVelocity*cos(p.initTwist))/(p.initPositionGFS(1));
-p.initVelocityGFS(3) = -(p.initVelocity*sin(p.initTwist))/(p.initPositionGFS(1));
+p.initVelocityGFS(2) = (p.initVelocity*cos( p.initTwist))/(p.initPositionGFS(1)*sin(p.initPositionGFS(3)));
+p.initVelocityGFS(3) = (p.initVelocity*sin(-p.initTwist))/(p.initPositionGFS(1));
 
-
+% r       = p.initPositionGFS(1);
+% theta   = p.initPositionGFS(2);
+% phi     = p.initPositionGFS(3);
+% 
+% rdot     = p.initVelocityGFS(1);
+% thetadot = p.initVelocityGFS(2);
+% phidot   = p.initVelocityGFS(3);
+% 
+% 
+% cart(1) =    cos(theta)*sin(phi)*rdot...
+%           -r*sin(theta)*sin(phi)*thetadot...
+%           +r*cos(theta)*cos(phi)*phidot;
+% 
+% cart(2) =    sin(theta)*sin(phi)*rdot...
+%           +r*cos(theta)*sin(phi)*thetadot...
+%           +r*sin(theta)*cos(phi)*phidot;
+% 
+% cart(3) = cos(phi)*rdot-r*sin(phi)*phidot;
+% 
+% cart
+% sqrt(sum(cart.^2))
 
 
 
--- scripts/plotInitialConditions.m
+++ scripts/plotInitialConditions.m
@@ -9,6 +9,12 @@ initVWindAppBFC = tsc.vWindAppBFC.data(1,:);
 initVelocityBFC = [p.initVelocity 0 0];
 initVWindBFC    = tsc.vWindBFC.data(1,:);
 
+initFWingBFC    = tsc.FWingBFC.data(1,:);
+initFWingBFC    = 5*initFWingBFC./sqrt(sum(initFWingBFC.^2));
+
+initFRudderBFC    = tsc.FRudderBFC.data(1,:);
+initFRudderBFC    = 5*initFRudderBFC./sqrt(sum(initFRudderBFC.^2));
+
 bfx=tsc.BFX_GFC.data(1,:);
 bfy=tsc.BFY_GFC.data(1,:);
 bfz=tsc.BFZ_GFC.data(1,:);
@@ -50,6 +56,9 @@ h.velocity     = plot3([0,initVelocityBFC(1)],[0,initVelocityBFC(2)],[0,initVelo
 h.apparentWind = plot3([initVelocityBFC(1),initVelocityBFC(1)+initVWindAppBFC(1)],...
                        [initVelocityBFC(2),initVelocityBFC(2)+initVWindAppBFC(2)],...
                        [initVelocityBFC(3),initVelocityBFC(3)+initVWindAppBFC(3)],'Color','g');
+h.fWing   = quiver3(0,0,0,initFWingBFC(1),initFWingBFC(2),initFWingBFC(3),'Color','c');
+h.fRudder = quiver3(0,0,0,initFRudderBFC(1),initFRudderBFC(2),initFRudderBFC(3),'Color','k');
+                   
 view([-75 30])
 axis equal
 xlabel('BFX')
